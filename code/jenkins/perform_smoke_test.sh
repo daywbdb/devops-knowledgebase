407e15fc05541af66826a7962b045355d1600eaa
filebat.mark@gmail.com <filebat.mark@gmail.com>
2015-08-16 11:50:44 +0800

    add script

---
+++ code/jenkins/perform_smoke_test.sh
@@ -0,0 +1,88 @@
+##-------------------------------------------------------------------
+## File : perform_smoke_test.sh
+## Author : Denny <denny.zhang001@gmail.com>
+## Description :
+## --
+## Created : <2015-08-16>
+## Updated: Time-stamp: <2015-08-16 11:50:24>
+##-------------------------------------------------------------------
+function log() {
+    local msg=$*
+    echo -ne `date +['%Y-%m-%d %H:%M:%S']`" $msg\n"
+}
+
+function prepare_protractor() {
+    working_dir=${1?}
+    shift
+    server_ip=${1?}
+    shift
+    test_case=$*
+    protractor_conf_cfg="$working_dir/protractor_conf.js"
+    protractor_testcase="$working_dir/protractor_testcase.js"
+
+    log "configure $protractor_conf_cfg"
+    cat > $protractor_conf_cfg <<EOF
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    // ----- What tests to run -----
+    specs: ['$protractor_testcase'],
+    // If you would like to run more than one instance of webdriver on the same
+    // tests, use multiCapabilities, which takes an array of capabilities.
+    // If this is specified, capabilities will be ignored.
+    multiCapabilities: [
+        {
+            'browserName': 'chrome',
+            'shardTestFiles': true,
+            'maxInstances': 10,
+            'acceptSslCerts': true,
+            'trustAllSSLCertificates': true
+        }
+    ],
+    // ----- Parameters for tests -----
+    params: {
+        login: {
+            server_ip: '$server_ip',
+        }
+    },
+
+    onPrepare: function() {
+        browser.driver.manage().window().setSize(1600, 800);
+    },
+
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 15000,
+        isVerbose: true,
+        includeStackTrace: true
+    },
+    // The timeout in milliseconds for each script run on the browser. This should
+    // be longer than the maximum time your application needs to stabilize between
+    // tasks.
+    allScriptsTimeout: 11000,
+
+    // How long to wait for a page to load.
+    getPageTimeout: 10000
+};
+EOF
+
+    log "configure $protractor_testcase"
+    cat > $protractor_testcase <<EOF
+describe('Authright GUI verification', function() {
+$test_case
+});
+EOF
+}
+
+function test_protractor() {
+    working_dir=${1?}
+    protractor_conf_cfg="$working_dir/protractor_conf.js"
+    protractor $protractor_conf_cfg
+}
+
+#################################################################################
+working_dir="/var/lib/jenkins/code/smoketest/"
+[ -d $working_dir ] || mkdir -p $working_dir
+
+prepare_protractor $working_dir "$server_ip" "$test_case"
+test_protractor $working_dir
+## File : perform_smoke_test.sh ends
