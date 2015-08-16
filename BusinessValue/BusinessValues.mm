b8230c5b6a2d02c912588a6bb2d10552ec7295ba
filebat.mark@gmail.com <filebat.mark@gmail.com>
2015-08-16 11:59:18 +0800

    add freemind

---
+++ BusinessValue/BusinessValues.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1439686973115" ID="ID_217404312" MODIFIED="1439687054894" TEXT="DevOps BusinessValues">
+<node CREATED="1439687002045" ID="ID_1977712800" MODIFIED="1439687002045" POSITION="right" TEXT=""/>
+</node>
+</map>
